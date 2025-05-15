eed1a69d5e95bd7fee43123a49481a080b9fb3a0
Nelofarkarimzada786 <karimzadanelofar@gmail.com>
2025-05-15 01:34:59 +0100

    2nd commit

--- src/main/java/class02/test01.java
+++ src/main/java/class02/test01.java
@@ -3,5 +3,11 @@ package class02;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+
     }
 }
